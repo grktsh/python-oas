5b122a001f8e732ed4d3707e639939a7d508e764
grktsh <grktsh@gmail.com>
2018-12-25 16:22:47 +0900

    Put falcon_oas package into src directory
    https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure

--- falcon_oas/extensions.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
-X_FALCON_RESOURCE = 'x-falcon-resource'
-X_USER_LOADER = 'x-user-loader'
