601936a8c8a7eba69ee419ccb4d6bb7202f6e43a
Atsushi Hanaoka <grktsh@gmail.com>
2019-04-27 00:43:07 +0900

    Export create_spec_from_dict and unmarshal_request (#2)

--- src/oas/__init__.py
+++ src/oas/__init__.py
@@ -4,3 +4,5 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from .request.models import Request  # noqa: F401
+from .request.unmarshalers import unmarshal_request  # noqa: F401
+from .spec import create_spec_from_dict  # noqa: F401
