13728cac4c978bc994fd67772671b91c7c6beef9
Atsushi Hanaoka <grktsh@gmail.com>
2019-08-15 02:05:31 +0900

    Export SchemaUnmarshaler (#14)

--- src/oas/__init__.py
+++ src/oas/__init__.py
@@ -5,4 +5,5 @@ from __future__ import unicode_literals
 
 from .request.models import Request  # noqa: F401
 from .request.unmarshalers import unmarshal_request  # noqa: F401
+from .schema.unmarshalers import SchemaUnmarshaler  # noqa: F401
 from .spec import create_spec_from_dict  # noqa: F401
